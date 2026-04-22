8c8888150722f1ed638c132912f3865dbe5f46ce
Sai_Vivek_Duvva <saivivekduvva@gmail.com>
2026-04-22 13:17:55 +0530

    Add emergency migration endpoint

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, ProfileView
 
+from django.http import HttpResponse
+from django.core.management import call_command
+import io
+
+def migrate_db(request):
+    output = io.StringIO()
+    try:
+        call_command('migrate', interactive=False, stdout=output)
+        return HttpResponse(f"Migration Successful!<br><pre>{output.getvalue()}</pre>")
+    except Exception as e:
+        return HttpResponse(f"Migration Failed!<br><pre>{str(e)}</pre>", status=500)
+
 urlpatterns = [
+    path('migrate-db-emergency/', migrate_db),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
