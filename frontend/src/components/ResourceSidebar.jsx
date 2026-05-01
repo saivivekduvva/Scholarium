5c64f1eff8460aba7f0eb787e1590e54274231f2
Sai_Vivek_Duvva <saivivekduvva@gmail.com>
2026-05-01 15:22:45 +0530

    Fix blank page on Launch Learning by adding missing motion import in ResourceSidebar

--- frontend/src/components/ResourceSidebar.jsx
+++ frontend/src/components/ResourceSidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { IoBookOutline, IoVideocamOutline, IoNewspaperOutline, IoExtensionPuzzleOutline } from 'react-icons/io5';
 
 const ResourceSidebar = ({ proSources, query, books }) => {
